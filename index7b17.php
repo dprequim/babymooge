fe10e8ab9ff8d54e484b500fc3a9c9496c3f18d6
Omaewa <35822384+dprequim@users.noreply.github.com>
2022-05-28 21:34:38 -0700

    Add files via upload

---
+++ index7b17.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en-gb">
+	<title type="text">AETERNA</title>
+	<subtitle type="text">Aeterna is a unique de-fi eco-system that offers a
+variety of ways to generate wealth and navigate
+your way through a confusing decentralized
+world.</subtitle>
+	<link rel="alternate" type="text/html" href="https://aeterna.eco"/>
+	<id>https://aeterna.eco/index.php</id>
+	<updated>2022-05-29T04:26:27+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="https://aeterna.eco/index.php?format=feed&amp;type=atom"/>
+</feed>
---
+++ indexc0d0.php
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>AETERNA</title>
+		<description><![CDATA[Aeterna is a unique de-fi eco-system that offers a
+variety of ways to generate wealth and navigate
+your way through a confusing decentralized
+world.]]></description>
+		<link>https://aeterna.eco/index.php</link>
+		<lastBuildDate>Sun, 29 May 2022 04:26:27 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="https://aeterna.eco/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+	</channel>
+</rss>
